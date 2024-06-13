c6f5941166fe9992e3b53abfd6f6fec16a10ab14
Axay17 <akshayteja04@gmail.com>
2024-06-14 01:10:15 +0530

    SignUp Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import Image from "next/image";
 import Navbar1 from "../components/Navbar1";
 import Login from "@/components/Login";
+import SignUp from "@/components/SignUp";
 
 export default function Home() {
   return (
     <main>
       <Navbar1 />
       <Login/>
+      <SignUp/>
     </main>
   );
 }
