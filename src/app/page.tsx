400bfbcf8c41045afe97dd1293b786e0d75fe79e
Axay17 <akshayteja04@gmail.com>
2024-06-14 23:17:13 +0530

    Landing Page Carousel added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,17 @@ import Navbar1 from "../components/Navbar";
 import Footer from "@/components/Footer";
 import Login from "@/components/Login";
 import SignUp from "@/components/SignUp";
+import LandingCarousel from "@/components/LandingCarousel";
 
 
 export default function Home() {
   return (
     <main>
       <div className="h-4/5">
-        Landing page
+      <LandingCarousel/>
+        <Login/>
+        <SignUp/>
+        
       </div>
     </main>
   );
---
+++ src/components/LandingCarousel.tsx
@@ -0,0 +1,46 @@
+"use client"
+import React from 'react';
+import Slider from "react-slick";
+import Slide from './Slide';
+
+const LandingCarousel = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    pauseOnHover: false,
+};
+
+const slideData = [
+    {
+        id: 0,
+        img: "/Carousel1.png",
+    },
+    {
+        id: 1,
+        img: "/Carousel2.png",
+    },
+    {
+        id: 2,
+        img: "/Carousel3.png",
+    },
+];
+
+return (
+    <div className='flex justify-center items-center'>
+        <div className='container m-3 mb-5 md:m-5'>
+            <Slider {...settings}>
+                    {slideData.map((item) => (
+                      <Slide key={item.id}
+                              img={item.img}
+                      />
+                  ))}
+            </Slider>
+        </div>
+    </div>
+)
+}
+
+export default LandingCarousel;
