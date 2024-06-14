5a5efca8cfc7821459410c2cfc5d3c79fd25b17c
GnananSai <pendyalagnanasai@gmail.com>
2024-06-14 11:55:39 +0530

    Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import Image from "next/image";
 import Navbar1 from "../components/Navbar1";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar1 />
+      <Footer/>
     </main>
   );
 }
