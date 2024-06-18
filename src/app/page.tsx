afed5fe46fe68761e7ff0cae9626d9bb3c6fee05
Axay17 <akshayteja04@gmail.com>
2024-06-18 23:13:53 +0530

    LandingHero component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,15 @@ import Footer from "@/components/Footer";
 import Login from "@/components/Login";
 import SignUp from "@/components/SignUp";
 import LandingCarousel from "@/components/LandingCarousel";
+import LandingHero from "@/components/LandingHero";
 
 
 export default function Home() {
   return (
     <main>
       <div className="h-4/5">
+      <LandingHero/>
       <LandingCarousel/>
-        <Login/>
-        <SignUp/>
-        
       </div>
     </main>
   );
