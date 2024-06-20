2fd57e3fe582f877e231c75fa67a41ac6059bc65
Axay17 <akshayteja04@gmail.com>
2024-06-20 14:39:52 +0530

    HomeHero & Homepage updated

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -13,30 +13,34 @@ const LandingHero = () => {
               Explore Communities and Events in your area. Connect with
               businesses and people in your community.
             </p>
-            <a
-              href="#"
-              className="flex items-center justify-start px-5 py-3 mr-3 text-base font-medium rounded-lg bg-primary-700 hover:bg-primary-800 mb-3 hover:bg-blue-gray-100 w-fit"
-            >
-              Get started
-              <svg
-                className="w-5 h-5 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </a>
-            <a
-              href="#"
-              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-800 border border-gray-800 rounded-lg hover:bg-blue-gray-800 hover:text-white focus:ring-4 focus:ring-gray-100 transition ease-in-out hover:scale-105"
-            >
-              Join Your CommUnity
-            </a>
+            <div className="flex gap-5">
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-gray-800 text-white border border-gray-800 rounded-lg hover:bg-white hover:text-gray-800 focus:ring-4 focus:ring-gray-100 transition ease-in-out hover:scale-105"
+                >
+                  What's New?
+                </a>
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-gray-800 text-white border border-gray-800 rounded-lg hover:bg-white hover:text-gray-800 focus:ring-4 focus:ring-gray-100 transition ease-in-out hover:scale-105"
+                >
+                  Discover Events
+                </a>
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-gray-800 text-white border border-gray-800 rounded-lg hover:bg-white hover:text-gray-800 focus:ring-4 focus:ring-gray-100 transition ease-in-out hover:scale-105"
+                >
+                  Buy/Sell
+                </a>
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-gray-800 text-white border border-gray-800 rounded-lg hover:bg-white hover:text-gray-800 focus:ring-4 focus:ring-gray-100 transition ease-in-out hover:scale-105"
+                >
+                  Avail Services
+                </a>
+            </div>
+            
+
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex m-5 transition ease-in-out hover:scale-105">
             <img
