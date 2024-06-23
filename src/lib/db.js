7efd19a29b91f7ec24631bbfd47909c78e2fb8b9
amarnathbhat <bhatamar8@gmail.com>
2024-06-23 16:48:44 +0530

    Setup: Database

---
+++ src/lib/db.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+global.mongoose = {
+  conn: null,
+  promise: null,
+};
+
+export async function dbConnect() {
+  try {
+    if (global.mongoose && global.mongoose.conn) {
+      console.log("Connected from previous");
+      return global.mongoose.conn;
+    } else {
+      const conString = process.env.MONGO_URI;
+
+      const promise = mongoose.connect(conString, {
+        autoIndex: true,
+      });
+
+      global.mongoose = {
+        conn: await promise,
+        promise,
+      };
+
+      console.log("Newly connected");
+      return await promise;
+    }
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw new Error("Database connection failed");
+  }
+}
+
+export const disconnect = () => {
+  if (!global.mongoose.conn) {
+    return;
+  }
+  global.mongoose.conn = null;
+  mongoose.disconnect();
+};
