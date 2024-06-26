5b6c64f35a13557eccc511e7f7399c3b2dac7aa0
GnananSai <pendyalagnanasai@gmail.com>
2024-06-26 15:26:11 +0530

    Upload widget

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "drxedpjdg",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
 
 export default nextConfig;
