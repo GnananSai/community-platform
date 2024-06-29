fb5ea043606fb25710b8405ad9d93cced346bee7
amarnathbhat <bhatamar8@gmail.com>
2024-06-29 17:14:31 +0530

    Update: Profile Edit API

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -4,14 +4,20 @@ import User from '@/models/User';
 
 export async function POST(req: Request) {
   await dbConnect();
+
   try {
     const data = await req.json();
-    const { id, ...userData } = data;
-    const user = await User.findByIdAndUpdate(id, userData, { new: true });
+    console.log(data);
+    const user = await User.findByIdAndUpdate(data.id, {$set: data.userDetails}, { new: true });
+    console.log(user);
+    if (!user) {
+      return NextResponse.json({ success: false, error: 'User not found' }, { status: 404 });
+    }
 
-    return NextResponse.json({ success: true, user: user });
-  } catch (error) {
-    return NextResponse.json({ success: false, error}, { status: 400 });
+    return NextResponse.json({ success: true, user });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ success: false, error: error.message || 'Something went wrong' }, { status: 400 });
   }
 }
 
--- src/components/Profile/ProfileEdit.tsx
+++ src/components/Profile/ProfileEdit.tsx
@@ -3,15 +3,16 @@ import React from "react";
 import { useUser } from "@/context/UserContext";
 import { useRouter } from "next/navigation";
 import { CldUploadWidget } from "next-cloudinary";
-import Upload from "@/components/Upload";
+
 const profileInitialValue = {
   name: "",
   gender: "",
-  dob: "",
+  dob: new Date(),
   city: "",
   state: "",
   country: "",
   image_url: "",
+  completedProfile: false,
 };
 
 const ProfileEdit = () => {
@@ -19,35 +20,46 @@ const ProfileEdit = () => {
   const router = useRouter();
   const [userDetails, setUserDetails] = React.useState(profileInitialValue);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserDetails({ ...userDetails, [name]: value });
+  };
+
   const handleUpload = (result: any) => {
     console.log("Upload Result:", result);
     if (result.event === "success" && result.info && result.info.secure_url) {
       const url = result.info.secure_url;
       console.log("Image URL:", url);
-      // setUserDetails({ ...userDetails, image_url: url });
+      setUserDetails({ ...userDetails, image_url: url });
     } else {
       console.error("Upload Error:", result.info);
     }
   };
 
-  const handleEdit = async () => {
+  const handleEdit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    userDetails.completedProfile = true;
     fetch("/api/profile", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(userDetails),
+      body: JSON.stringify({ userDetails, id: user._id }),
     })
       .then((res) => res.json())
       .then((data) => {
         if (data.success) {
           setUser(data.user);
+          console.log("User:", data.user);
           router.replace("/home");
         } else {
-          alert(data.message);
+          alert(data.error);
+          console.error(data.error);
         }
       })
-      .catch((err) => alert(err.message));
+      .catch((err) => alert(err));
   };
 
   return (
@@ -56,47 +68,45 @@ const ProfileEdit = () => {
         Edit Your Profile
       </h1>
       <article className="flex flex-col gap-3 justify-center items-center">
-            <div className="relative group w-32 h-32 mb-4 ">
-              <img
-                src={user.image_url ? user.image_url : "/profile.webp"}
-                className="w-full h-full bg-gray-300 rounded-full object-cover transition duration-300 ease-in-out group-hover:opacity-50 shadow-md shadow-black"
-                alt="Profile Pic"
-              />
-              <CldUploadWidget
-                uploadPreset="ml_default"
-                onSuccess={(result) => {
-                  handleUpload(result);
-                }}
-                onError={(error) => {
-                  console.error("Upload Error:", error);
-                }}
+        <div className="relative group w-32 h-32 mb-4 ">
+          <img
+            src={userDetails.image_url || user.image_url || "/profile.webp"}
+            className="w-full h-full bg-gray-300 rounded-full object-cover transition duration-300 ease-in-out group-hover:opacity-50 shadow-md shadow-black"
+            alt="Profile Pic"
+          />
+          <CldUploadWidget
+            uploadPreset="ml_default"
+            onSuccess={(result) => {
+              handleUpload(result);
+            }}
+            onError={(error) => {
+              console.error("Upload Error:", error);
+            }}
+          >
+            {({ open }) => (
+              <button
+                onClick={() => open()}
+                className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer"
               >
-                {({ open }) => (
-                  <button
-                    onClick={() => open()}
-                    className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="50"
-                      height="50"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"
-                      ></path>
-                    </svg>
-                  </button>
-                )}
-              </CldUploadWidget>
-              
-            </div>
-        
-        <form onSubmit={handleEdit} className="flex flex-col gap-3">
-          <div className="flex flex-col items-center justify-center">
-          </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"
+                  ></path>
+                </svg>
+              </button>
+            )}
+          </CldUploadWidget>
+        </div>
+
+        <form className="flex flex-col gap-3">
+          <div className="flex flex-col items-center justify-center"></div>
           <table className="w-full">
             <tbody className="space-y-10">
               <tr className="">
@@ -111,8 +121,10 @@ const ProfileEdit = () => {
                 <td className="">
                   <input
                     type="text"
+                    onChange={(e) => handleChange(e)}
                     id="name"
                     name="name"
+                    required
                     className="w-full px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-800"
                     placeholder="Enter your name"
                   />
@@ -134,6 +146,7 @@ const ProfileEdit = () => {
                     <label className="flex items-center text-gray-800 rounded-md px-3 py-2 hover:text-white hover:bg-gray-800 cursor-pointer">
                       <input
                         type="radio"
+                        onChange={(e) => handleChange(e)}
                         name="gender"
                         value="male"
                         className="mr-2"
@@ -144,6 +157,7 @@ const ProfileEdit = () => {
                       <input
                         type="radio"
                         name="gender"
+                        onChange={(e) => handleChange(e)}
                         value="female"
                         className="mr-2"
                       />
@@ -152,6 +166,7 @@ const ProfileEdit = () => {
                     <label className="flex items-center text-gray-800 rounded-md px-3 py-2 hover:text-white hover:bg-gray-800 cursor-pointer">
                       <input
                         type="radio"
+                        onChange={(e) => handleChange(e)}
                         name="gender"
                         value="other"
                         className="mr-2"
@@ -161,6 +176,7 @@ const ProfileEdit = () => {
                     <label className="flex items-center text-gray-800 rounded-md px-3 py-2 hover:text-white hover:bg-gray-800 cursor-pointer">
                       <input
                         type="radio"
+                        onChange={(e) => handleChange(e)}
                         name="gender"
                         value="not_say"
                         className="mr-2"
@@ -183,6 +199,7 @@ const ProfileEdit = () => {
                 <td className="">
                   <input
                     type="date"
+                    onChange={(e) => handleChange(e)}
                     id="dob"
                     name="dob"
                     className="w-full px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-800"
@@ -204,6 +221,7 @@ const ProfileEdit = () => {
                   <input
                     type="text"
                     id="city"
+                    onChange={(e) => handleChange(e)}
                     name="city"
                     className="w-full px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-800"
                     placeholder="City Name"
@@ -223,6 +241,7 @@ const ProfileEdit = () => {
                 <td className="">
                   <input
                     type="text"
+                    onChange={(e) => handleChange(e)}
                     id="state"
                     name="state"
                     className="w-full px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-800"
@@ -243,6 +262,7 @@ const ProfileEdit = () => {
                 <td className="">
                   <input
                     type="text"
+                    onChange={(e) => handleChange(e)}
                     id="country"
                     name="country"
                     className="w-full px-3 py-2 border border-gray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-gray-800"
@@ -254,6 +274,7 @@ const ProfileEdit = () => {
               <tr className="">
                 <td colSpan={2} className="text-center">
                   <button
+                    onClick={(e) => handleEdit(e)}
                     type="submit"
                     className="m-3 bg-blue-gray-800 text-white px-3 py-2 rounded-md hover:bg-white hover:text-gray-800 hover:border hover:border-blue-gray-800 focus:outline-none focus:ring-gray-900 transition ease-in-out hover:scale-105"
                   >
